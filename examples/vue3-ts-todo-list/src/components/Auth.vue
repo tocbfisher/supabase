c86c0daaf55d6660fa08ac8029705a15db5a5980
tocbfisher <bfisher@toclogistics.com>
2021-02-18 02:05:36 -0500

    Update Auth.vue

--- examples/vue3-ts-todo-list/src/components/Auth.vue
+++ examples/vue3-ts-todo-list/src/components/Auth.vue
@@ -54,11 +54,11 @@
           <div class="mt-6">
             <span class="block w-full rounded-md shadow-sm">
               <button
-                @click="handleOAuthLogin('github')"
+                @click="handleOAuthLogin('azure')"
                 type="button"
                 class="w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition duration-150 ease-in-out"
               >
-                GitHub
+                Azure
               </button>
             </span>
           </div>
